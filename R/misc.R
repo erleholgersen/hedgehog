4c2652733fbb5a8abf4c172f89b7a3570b810f2e
wuergh <erle.holgersen@gmail.com>
2015-11-26 12:44:33 -0500

    Added Latin Hypercube Sampling functions

--- R/misc.R
+++ R/misc.R
@@ -1,3 +1,5 @@
+# Helper functions with no clear category
+
 factor.to.numeric <- function(x) {
   return(as.numeric(as.character(x)))
 }
--- R/monte-carlo.R
+++ R/monte-carlo.R
@@ -25,3 +25,64 @@ geometric_mean <- function(numbers) {
   return(prod(numbers)^(1/n))
 }
 
+
+rnorm_correlated <- function(n, rho) {
+  # generates samples from two correlated standard normal variables
+  # n number of samples to be generated from each variable.
+  # rho correlation between variables
+
+  n_assets = 2
+
+  dW = rnorm(n_assets*n)
+  dim(dW) = c(n, n_assets)
+
+  correlation_matrix = array(rho, dim = c(n_assets, n_assets))
+  diag(correlation_matrix) = 1
+
+  cholesky_factor = chol(correlation_matrix)
+
+  dW = dW %*% cholesky_factor
+
+  return(dW)
+}
+
+
+lhs <- function(s, n) {
+  # Latin Hypercube Sampling
+  # s number of dimensions
+  # n number of points in each dimension
+
+  minimums = 0:(n - 1)/n
+  maximums = 1:n/n
+
+  X = runif(s*n, minimums, maximums)
+  dim(X) = c(n, s)
+
+  # randomly reorder columns independently
+  for(i in 1:s) {
+    row_order = sample(1:n, n)
+    X[,i] = X[row_order, i]
+  }
+  return(X)
+}
+
+lhs_rnorm_correlated <- function(n, rho) {
+  # generates samples from two correlated standard normal variables with Latin Hypercube Sampling
+  # n number of samples to be generated from each variable.
+  # rho correlation between variables
+
+  n_assets = 2
+
+  uniforms = lhs(n_assets, n)
+
+  dW = qnorm(uniforms)
+  correlation_matrix = array(rho, dim = c(n_assets, n_assets))
+  diag(correlation_matrix) = 1
+
+  cholesky_factor = chol(correlation_matrix)
+
+  dW = dW %*% cholesky_factor
+
+  return(dW)
+
+}
