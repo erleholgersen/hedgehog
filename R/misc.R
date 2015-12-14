e0efb313c37120a1cba90c804bce1354af5dd78d
wuergh <erle.holgersen@gmail.com>
2015-12-13 22:56:15 -0500

    Added interleave function

--- R/misc.R
+++ R/misc.R
@@ -3,3 +3,17 @@
 factor.to.numeric <- function(x) {
   return(as.numeric(as.character(x)))
 }
+
+interleave <- function(x, y) {
+  min_length = min(length(x), length(y))
+
+  interleaved = as.numeric(rbind(x[1:min_length], y[1:min_length]))
+
+  if(length(x) > min_length) {
+    interleaved = c(interleaved, x)
+  } else if(length(y) > min_length) {
+    interleaved = c(interleaved, y)
+  }
+
+  return(interleaved)
+}
