0039490e53109ef5b41e37edd7fbe80a7d0f165a
wuergh <erle.holgersen@gmail.com>
2017-02-28 13:53:26 -0500

    Added head.cat function, and updated description

---
+++ R/head_cat.R
@@ -0,0 +1,34 @@
+#' Print head of an arbitrary number of objects.
+#' 
+#' @param ... objects to print head of
+#' 
+#' @return None
+#' 
+#' @examples
+#' a <- 1:100;
+#' b <- 50:100;
+#' head.cat(a, b, letters);
+#' 
+#' @export head.cat
+head.cat <- function(...) {
+  
+  # get all arguments
+  arguments <- list(...);
+  
+  # get names of aforementioned variables
+  variable.names <- as.list(match.call())[-1]; # first element in match.call is name of function
+  names(arguments) <- unlist(variable.names);
+  
+  # loop over variables and print head. 
+  for (variable.name in names(arguments)) {
+    
+    # show which variable is being printed
+    cat('Variable:', variable.name, '\n');
+    
+    # print head of variable
+    print( head(arguments[[variable.name]]) );
+    
+    # extra line space
+    cat('\n');
+  }
+}
