78fe96311e1e6da1f8de53df53ba181fce1568dc
wuergh <erle.holgersen@gmail.com>
2017-03-02 11:23:22 -0500

    Function for date-stamping filenames

---
+++ R/datestamp_filename.R
@@ -0,0 +1,35 @@
+#' Generate date-stamped filename
+#'
+#' @param file.name core part of file name to be date stamped
+#' @param extension extension
+#'
+#' @return stamped.file.name generated file name
+#'
+#' @export datestamp.filename
+datestamp.filename <- function(file.name, extension, timestamp = FALSE) {
+    
+    # get stamp 
+    if (timestamp) {
+        
+        # if timestamping requested, include both date and time
+        stamp <- format(
+            Sys.time(),
+            '%Y-%m-%d-%H:%M:%S'
+            );
+        
+    } else {
+        # by default, only include date
+        stamp <- Sys.Date();
+    }
+
+    # merge components
+    stamped.file.name <- paste0(
+      stamp, 
+      '_',
+      file.name,
+      '.',
+      extension
+    );
+    
+    return(stamped.file.name);
+}
