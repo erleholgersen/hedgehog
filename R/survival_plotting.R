a9b50a101da4cc113805f798c006276c09e78f76
wuergh <erle.holgersen@gmail.com>
2018-01-01 00:15:30 +0100

    Fixed risk table issue

--- R/survival_plotting.R
+++ R/survival_plotting.R
@@ -19,7 +19,20 @@ km.plot <- function(km.formula, dataset, show.risk.table = FALSE) {
   x.max <- max( dataset[, all.vars(km.formula)[1]]);
   
   ### BUILD PLOT ##############################################################
-
+  
+  strata.labels <- unique(summary(km.fit)$strata);
+  
+  strata.widths <- sapply(
+     as.character(strata.labels),
+     strwidth, 
+     units = 'inches'
+     );
+  
+  # set margins
+  par(
+      mai = c(0.8 + 0.2*length(strata.labels), 0.4 + max(strata.widths), 0.82, 0.42)
+      );
+  
   plot(km.fit);
   
   if(show.risk.table) {
@@ -30,15 +43,45 @@ km.plot <- function(km.formula, dataset, show.risk.table = FALSE) {
     risk.data <- summary(
       km.fit, 
       times = risk.times
-    );
+      );
     
+    print(risk.data);
+    
+    unique.strata <- unique(risk.data$strata);
+    
+    n.risk <- split(risk.data$n.risk, risk.data$strata);
+    
+    for(i in 1:length(n.risk) ) {
+        strata <- names(n.risk)[i];
+        print(strata);
+        
+        # pad with zeroes at the end
+        strata.risk <- c(
+            n.risk[[ i ]],
+            rep(0, length(risk.times) - length(n.risk[[ i ]] ))
+            );
+        
+        print(strata.risk);
+        
+        mtext(
+            strata,
+            side = 1,
+            at = -0.3*risk.times[2],
+            adj = 1,
+            line = 2 + i, 
+            font = 2,
+            );
+        
+        mtext(
+            strata.risk,
+            side = 1,
+            at = risk.times,
+            line = 2 + i
+        );
+        
+    }
     # show risk table
-    mtext(
-      risk.data$n.risk,
-      side = 1,
-      at = risk.times,
-      line = 3
-    );
+
     
   }
   
