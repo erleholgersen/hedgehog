5a3a31de524296905dc1bf580ec86b3a6545cb32
wuergh <erle.holgersen@gmail.com>
2018-05-21 21:15:29 +0100

    Added format.p.value function

---
+++ R/format.p.value.R
@@ -0,0 +1,23 @@
+#' format.p.value
+#' 
+#' @description 
+#' Format a p-value for a plot
+#'
+#' @param p.value p-value to be formatted
+#' @param prefix prefix for p-value, defaults to "P"
+#' 
+#' @return language object with p-value expression
+#'
+#' @export format.p.value
+format.p.value <- function(p.value, prefix = 'P') {
+    
+    if( p.value < .Machine$double.eps ) {
+        p.expression <- bquote( .(prefix) < .( scientific.notation(.Machine$double.eps)) );
+    } else if( p.value < 0.001) {
+        p.expression <- bquote( .(prefix) == .( scientific.notation( p.value) ) );
+    } else {
+        p.expression <- bquote( .(prefix) == .(p.value) );
+    }
+    
+    return(p.expression);
+}
